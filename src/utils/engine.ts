bfce4a359114f6749cd64bfe26999945620969fb
Mario Bandeira <josemariobandeira@hotmail.com>
2023-11-29 16:56:56 -0300

    feat: implements the math expression generator functions

---
+++ src/utils/engine.ts
@@ -0,0 +1,25 @@
+import { randomInt } from 'mathjs';
+
+const get_number = (len: number) => {
+	const matrix: any = {
+		1: [0, 9],
+		2: [10, 99],
+		3: [100, 999],
+		4: [0, 9999]
+	};
+
+	const index = len;
+	const [min, max] = matrix[index];
+	return randomInt(min, max);
+};
+
+const get_operator = (family: number) => {
+	const familyOne = ['+', '-'];
+	const familyTwo = ['*', '/'];
+	const sorted = randomInt(0, 100);
+	const index = sorted >= 50 ? 1 : 0;
+
+	return family === 1 ? familyOne[index] : familyTwo[index];
+};
+
+export { get_number, get_operator };
