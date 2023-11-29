7ea1b9cf79476096da5abc8371a47177ac0c987a
Mario Bandeira <josemariobandeira@hotmail.com>
2023-11-29 17:05:03 -0300

    feat: new home view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 import BoardView from '@/views/BoardView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import { eval_expression, get_number, get_operator } from '@/utils/engine';
+
+const question = ref<string>();
+const response = ref<string>();
+
+function generate() {
+	const exp = `${get_number(1)} ${get_operator(1)} ${get_number(1)}`;
+	question.value = exp;
+	response.value = eval_expression(exp);
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="game-view-home">
+		<div class="expression">{{ question }} = {{ response }}</div>
+		<div class="button">
+			<button @click="generate">Generate</button>
+		</div>
   </main>
 </template>
+
+<style>
+.game-view-home {
+	display: flex;
+	flex-wrap: wrap;
+	width: 640px;
+	height: 400px;
+	padding: 1em;
+	background-color: #adadad;
+}
+
+.game-view-home > .expression {
+	width: 100%;
+	align-self: flex-start;
+}
+
+.game-view-home > .buttons {
+	width: 100%;
+	align-self: flex-end;
+}
+</style>
